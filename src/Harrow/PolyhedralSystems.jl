0de9835a86abb086b13a7dc0895342b57b0e97ff
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-03-29 18:20:13 -0400

    Generalized field -> ring

--- src/Harrow/PolyhedralSystems.jl
+++ src/Harrow/PolyhedralSystems.jl
@@ -1,50 +1,51 @@
 # Code for setting up equations arising from commutative diagrams
+# We allow the equations to be set up over a ring for generality
 
 # Pentagon equations 
 export pentagon_equations
 
-# default field is algebraic_closure of ℚ
+# default ring is algebraic_closure of ℚ
 function pentagon_equations(fr::FusionRing)::PolSys
     pentagon_equations( fr, ℚb )
 end
 
-function pentagon_equations(fr::FusionRing, k::Field )::PolSys
+function pentagon_equations(fr::FusionRing, k::Ring )::PolSys
     multiplicity(fr) === 1 ? mf_pent_eqns(fr,k) : pent_eqns(fr,k) 
 end
 
 # multiplicity-free pentagon equations 
-function mf_pent_eqns(fr::FusionRing,k::Field)::PolSys
+function mf_pent_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
 
     #TODO: implement
 end
 
 # pentagon equations with multiplicity
-function pent_eqns(fr::FusionRing,k::Field)::PolSys
+function pent_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
     #TODO: implement
 end
 
 export hexagon_equations
 
-# default field is algebraic_closure of ℚ
+# default ring is algebraic_closure of ℚ
 function hexagon_equations(fr::FusionRing)::PolSys
     hexagon_equations( fr, ℚb )
 end
 
-function hexagon_equations(fr::FusionRing, k::Field )::PolSys
+function hexagon_equations(fr::FusionRing, k::Ring )::PolSys
     multiplicity(fr) === 1 ? mf_hex_eqns(fr,k) : hex_eqns(fr,k) 
 end
 
 # multiplicity-free hexagon equations 
-function mf_hex_eqns(fr::FusionRing,k::Field)::PolSys
+function mf_hex_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
 
     #TODO: implement
 end
 
 # hexagon equations with multiplicity
-function hex_eqns(fr::FusionRing,k::Field)::PolSys
+function hex_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
 
     #TODO: implement
@@ -53,23 +54,23 @@ end
 
 export pivotal_equations
 
-# default field is algebraic_closure of ℚ
+# default ring is algebraic_closure of ℚ
 function hexagon_equations(fr::FusionRing)::PolSys
     pivotal_equations( fr, ℚb )
 end
 
-function pivotal_equations(fr::FusionRing, k::Field )::PolSys
+function pivotal_equations(fr::FusionRing, k::Ring )::PolSys
     multiplicity(fr) === 1 ? mf_piv_eqns(fr,k) : piv_eqns(fr,k) 
 end
 
 # multiplicity-free pivotal equations 
-function mf_piv_eqns(fr::FusionRing,k::Field)::PolSys
+function mf_piv_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
     #TODO: implement
 end
 
 # pivotal equations with multiplicity
-function piv_eqns(fr::FusionRing,k::Field)::PolSys
+function piv_eqns(fr::FusionRing,k::Ring)::PolSys
     print("Not implemented yet.")
     #TODO: implement
 end
