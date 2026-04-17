c98ea042386ce96f32cda3ab671df98e8c8f6907
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-04-16 23:16:07 -0400

    + Inequality struct

--- src/Gideon/Structs.jl
+++ src/Gideon/Structs.jl
@@ -47,3 +47,66 @@ end
 # TODO: implement ParametrizedPolSys for polynomial systems that can be iterated over but 
 # are too large to store all at once. This would be useful for when we solve a subsystem using the smith decomposition
 # and we get a large amount of solutions. 
+
+# Inequality of the form pol <s> 0 where s can be
+# 1 : >, 
+# 2 : ≥, 
+# 3 : ≠
+
+struct Inequality
+  rng::Ring
+  pol
+  t::Int64
+end
+
+function inequality( ring, polynomial, type )
+  s = if type == >
+    1
+  elseif type == ≥
+    2
+  elseif type == ≠
+    3
+  end
+
+  Inequality( ring, polynomial, s )
+end
+
+polynomial( ineq::Inequality ) = ineq.pol
+
+type( ineq::Inequality ) = ineq.t
+
+ring( ineq::Inequality ) = ineq.rng
+
+function Base.show( io::IO, ineq::Inequality )
+  t = type(ineq)
+  s = if t == 1
+    " > "
+  elseif t == 2
+    " ≥ "
+  elseif t == 3
+    " ≠ "
+  end
+  print( io, string(polynomial(ineq)) * s * "0" )
+end
+
+function evaluate( ineq::Inequality, vars::Vector{Int}, vals::Vector{T} ) where {T <: RingElement}
+  inequality(
+    ring(ineq),
+    evaluate( polynomial(ineq), vars, vals ),
+    type(ineq)
+  )
+end
+
+function is_true( ineq::Inequality ) 
+  p = polynomial(ineq)
+  !is_constant(p) && return false
+  
+  t = type(ineq)
+  if t == 1
+    return p > 0 
+  elseif t == 2
+    return p >= 0
+  else 
+    return p != 0
+  end 
+end
