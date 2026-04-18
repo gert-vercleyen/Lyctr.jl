f6345cd29264e1d9c75c6c29a23af13d172ceeb5
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-04-17 23:22:41 -0400

    +proper initialization fields

--- src/Lyctr.jl
+++ src/Lyctr.jl
@@ -8,6 +8,13 @@ include("general_functions.jl")
 include("Gideon/Gideon.jl")
 include("Harrow/Harrow.jl")
 
-# Write your package code here.
+function __init__()
+    # GLOBAL VARIABLES
+    # Abreviations for commonly used fields
+    ℚ = QQ;
+    ℚb       = algebraic_closure(ℚ)
+    ℚab, ζ   = abelian_closure(ℚ; sparse = false)
+    sℚab, ζs = abelian_closure(ℚ; sparse = true)
+end
 
 end
