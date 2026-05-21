1b1fd536cbfd64b17e5003540be0ea868100c2f9
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-05-20 22:07:28 -0400

    +includes

--- src/Gideon/Gideon.jl
+++ src/Gideon/Gideon.jl
@@ -1,3 +1,6 @@
 
-include("Structs.jl")
-include("PolynomialSystems.jl")
+include("structs.jl")
+include("towers.jl")
+include("matrix_decompositions.jl")
+include("polynomial_systems.jl")
+include("reducing_systems.jl")
