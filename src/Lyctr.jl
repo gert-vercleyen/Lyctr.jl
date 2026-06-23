a05465f60637376cb5d019358b50a48340d75fcf
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-06-23 12:14:13 +0200

    +import statements

--- src/Lyctr.jl
+++ src/Lyctr.jl
@@ -3,7 +3,10 @@ module Lyctr
 using Oscar
 import Oscar: evaluate, polynomial_ring, sparse_matrix, snf
 using FusionRings
+import FusionRings: fusion_ring, multiplication_table, frl, fawc
 using PicoSAT
+using TensorCategories
+import TensorCategories: F_symbols, R_symbols, P_symbols, anyonwiki, multiplication_table
 
 include("general_functions.jl")
 include("Gideon/Gideon.jl")
