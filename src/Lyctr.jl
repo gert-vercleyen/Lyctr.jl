64a1b39e7970f13fe173f5690ca0182b600e5a97
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-08-26 17:45:56 +0200

    +initialization

--- src/Lyctr.jl
+++ src/Lyctr.jl
@@ -22,11 +22,63 @@ include("general_functions.jl")
 include("Gideon/Gideon.jl")
 include("Harrow/Harrow.jl")
 
+export QQb, QQab, ζ, QQabs, QQemb 
 function __init__()
-  global QQb     = algebraic_closure(QQ)
-  global QQab, ζ = abelian_closure(QQ)
-    # GLOBAL VARIABLES
-    # Abreviations for commonly used fields
+  global QQb      = algebraic_closure(QQ)
+  global QQab, ζ  = abelian_closure(QQ)
+  # Following is necessary to work with trivial embeddings from ℚ → ℂ
+  global QQabs, _ = rationals_as_number_field()
+  global QQemb    = hom( QQabs, QQb, QQb(1) )
+
+  # GLOBAL VARIABLES
+  # Abreviations for commonly used fields
+
+  # importing data 
+  global datadir = joinpath(@__DIR__,"data")
+  fns = readdir(datadir)
+
+  if "split_number_data" ∉ fns
+    println("Dataset of numbers not yet optimized. Optimizing for future use.")
+
+    # Create directory for numbers
+    splitdatapath = joinpath(datadir, "split_number_data/")
+    qqbpath       = joinpath(splitdatapath,"algebraic_numbers")
+    asnpath       = joinpath(splitdatapath,"abstract_numbers")
+
+    mkdir(splitdatapath)
+    mkdir(qqbpath)
+    mkdir(asnpath)
+
+    println("Importing numbers.")
+    # Import qqb numbers from big files
+    qqb_nums = begin
+      ids  = Oscar.load(joinpath(datadir, "qqb_ids.mrdi"))
+      nums = Oscar.load(joinpath(datadir, "qqb_vals.mrdi"))
+
+      [ids[i], nums[i] for i in 1:length(ids)]
+    end
+    #=
+    absnumfieldelems = begin
+      nothing
+    end 
+
+    embeddings = begin
+      nothing
+    end
+    =#
+    println("Exporting numbers separately.")
+    # Export qqb numbers
+    function exportnum(tuple)
+      dir = joinpath(datadir, "split_number_data/", tuple[1]*".mrdi")
+      return Oscar.save(path, tuple[2])
+    end
+
+    exportnum.(qqb_nums)
+    println("Dataset is optimized.")
+  end
+
+  # start an empty qqb_dict which grows on demand 
+  global qqb_dict = Dict{String, QQBarFieldElem}()
 end
 
 end
